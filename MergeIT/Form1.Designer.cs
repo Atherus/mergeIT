0776925dfa6d5454974d4991729c6e32f812189c
Peter Meluš <meluspe@gmail.com>
2016-05-16 14:28:57 +0200

    Init App

---
+++ MergeIT/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MergeIT {
+    partial class Form1 {
+        /// <summary>
+        /// Vyžaduje se proměnná návrháře.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Uvolněte všechny používané prostředky.
+        /// </summary>
+        /// <param name="disposing">hodnota true, když by se měl spravovaný prostředek odstranit; jinak false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kód generovaný Návrhářem Windows Form
+
+        /// <summary>
+        /// Metoda vyžadovaná pro podporu Návrháře - neupravovat
+        /// obsah této metody v editoru kódu.
+        /// </summary>
+        private void InitializeComponent() {
+            this.listBoxFiles = new System.Windows.Forms.ListBox();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.buttonMerge = new System.Windows.Forms.Button();
+            this.labelVersion = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxFiles
+            // 
+            this.listBoxFiles.AllowDrop = true;
+            this.listBoxFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxFiles.FormattingEnabled = true;
+            this.listBoxFiles.Location = new System.Drawing.Point(13, 13);
+            this.listBoxFiles.Name = "listBoxFiles";
+            this.listBoxFiles.Size = new System.Drawing.Size(354, 199);
+            this.listBoxFiles.TabIndex = 0;
+            this.listBoxFiles.DragEnter += new System.Windows.Forms.DragEventHandler(this.listBoxFiles_DragEnter);
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonUp.Location = new System.Drawing.Point(373, 12);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(75, 25);
+            this.buttonUp.TabIndex = 1;
+            this.buttonUp.Text = "Hore";
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonDown.Location = new System.Drawing.Point(373, 43);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(75, 25);
+            this.buttonDown.TabIndex = 2;
+            this.buttonDown.Text = "Dole";
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // buttonMerge
+            // 
+            this.buttonMerge.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.buttonMerge.Location = new System.Drawing.Point(13, 218);
+            this.buttonMerge.Name = "buttonMerge";
+            this.buttonMerge.Size = new System.Drawing.Size(75, 23);
+            this.buttonMerge.TabIndex = 3;
+            this.buttonMerge.Text = "Spojiť";
+            this.buttonMerge.UseVisualStyleBackColor = true;
+            this.buttonMerge.Click += new System.EventHandler(this.buttonMerge_Click);
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelVersion.AutoSize = true;
+            this.labelVersion.Location = new System.Drawing.Point(388, 228);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(60, 13);
+            this.labelVersion.TabIndex = 4;
+            this.labelVersion.Text = "Version 0.1";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonClear.Location = new System.Drawing.Point(373, 189);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 5;
+            this.buttonClear.Text = "Vyčistiť";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 250);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.labelVersion);
+            this.Controls.Add(this.buttonMerge);
+            this.Controls.Add(this.buttonDown);
+            this.Controls.Add(this.buttonUp);
+            this.Controls.Add(this.listBoxFiles);
+            this.MinimumSize = new System.Drawing.Size(432, 285);
+            this.Name = "Form1";
+            this.Text = "MergeIT";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxFiles;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonMerge;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ MergeIT/Form1.cs
@@ -0,0 +1,108 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace MergeIT {
+    public partial class Form1 : Form {
+
+
+        public Form1() {
+            InitializeComponent();
+            listBoxFiles.DragDrop += listBoxFiles_DragDrop;
+            listBoxFiles.DragEnter += listBoxFiles_DragEnter;
+        }
+
+        private void listBoxFiles_DragDrop(object sender, DragEventArgs e) {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            foreach (string file in files)
+                listBoxFiles.Items.Add(file);
+        }
+
+        private void listBoxFiles_DragEnter(object sender, DragEventArgs e) {
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effect = DragDropEffects.Copy;
+        }
+
+        private void buttonUp_Click(object sender, EventArgs e) {
+            MoveItem(-1);
+        }
+
+        private void buttonDown_Click(object sender, EventArgs e) {
+            MoveItem(1);
+        }
+
+        public void MoveItem(int direction) {
+            if (listBoxFiles.SelectedItem == null || listBoxFiles.SelectedIndex < 0) {
+                return;
+            }
+
+            int newIndex = listBoxFiles.SelectedIndex + direction;
+            if (newIndex < 0 || newIndex >= listBoxFiles.Items.Count) {
+                return;
+            }
+
+            object selected = listBoxFiles.SelectedItem;
+            listBoxFiles.Items.Remove(selected);
+            listBoxFiles.Items.Insert(newIndex, selected);
+            listBoxFiles.SetSelected(newIndex, true);
+        }
+
+        private void buttonMerge_Click(object sender, EventArgs e) {
+            if (listBoxFiles.Items.Count > 0) {
+                byte[] mergedPdf = null;
+                using (MemoryStream ms = new MemoryStream()) {
+                    using (Document document = new Document()) {
+                        using (PdfCopy copy = new PdfCopy(document, ms)) {
+                            document.Open();
+
+                            for (int i = 0; i < listBoxFiles.Items.Count; ++i) {
+                                PdfReader reader = new PdfReader(listBoxFiles.Items[i].ToString());
+                                // loop over the pages in that document
+                                int n = reader.NumberOfPages;
+                                for (int page = 0; page < n;) {
+                                    copy.AddPage(copy.GetImportedPage(reader, ++page));
+                                }
+                            }
+                        }
+                    }
+                    mergedPdf = ms.ToArray();
+                    ByteArrayToFile(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\genPDF.pdf", mergedPdf);
+                }
+            }  
+        }
+
+        public bool ByteArrayToFile(string _FileName, byte[] _ByteArray) {
+            try {
+                // Open file for reading
+                System.IO.FileStream _FileStream =
+                   new System.IO.FileStream(_FileName, System.IO.FileMode.Create,
+                                            System.IO.FileAccess.Write);
+                // Writes a block of bytes to this stream using data from
+                // a byte array.
+                _FileStream.Write(_ByteArray, 0, _ByteArray.Length);
+
+                // close file stream
+                _FileStream.Close();
+
+                return true;
+            } catch (Exception _Exception) {
+                // Error
+                Console.WriteLine("Exception caught in process: {0}",
+                                  _Exception.ToString());
+            }
+
+            // error occured, return false
+            return false;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e) {
+            listBoxFiles.Items.Clear(); 
+        }
+    }
+
+    
+}
